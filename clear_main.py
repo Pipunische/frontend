b23c2628a966fb32c0ae8f85c8486d4a9c1b1327
ARS <gulyayarseniy2022@gmail.com>
2026-07-14 08:50:55 +0300

    feat: final version, before all bug fixed

--- clear_main.py
+++ clear_main.py
@@ -1,7 +1,7 @@
+from multiprocessing.spawn import old_main_modules
 import time
 import os
 import uuid
-import boto3
 import requests
 from fastapi import FastAPI, Request, Form, UploadFile, File
 from fastapi.templating import Jinja2Templates
@@ -10,18 +10,6 @@ from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from starlette.middleware.sessions import SessionMiddleware
 
-
-S3_ENDPOINT = os.getenv("S3_ENDPOINT", "https://yourendpoint.r2.cloudflarestorage.com")
-S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY", "your_access_key")
-S3_SECRET_KEY = os.getenv("S3_SECRET_KEY", "your_secret_key")
-S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "polupoker-avatars")
-
-s3_client = boto3.client(
-    's3',
-    endpoint_url=S3_ENDPOINT,
-    aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_KEY
-)
 
 app = FastAPI()
 
@@ -32,8 +20,8 @@ app.add_middleware(SessionMiddleware, secret_key="alexei_pipunesco", max_age=604
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-JAVA_HOST = "192.168.0.68"
-JAVA_URL = f"http://{JAVA_HOST}:8080/api/tables"
+JAVA_HOST = "amenity-lard-screen.ngrok-free.dev"
+JAVA_URL = f"https://{JAVA_HOST}/api/tables"
 
 
 def extract_cards(player: dict) -> list:
@@ -41,6 +29,21 @@ def extract_cards(player: dict) -> list:
     if isinstance(cards, list):
         return cards
     return []
+
+
+class CreateTableRequest(BaseModel):
+    name: str
+    passcode: str | None = None
+    chips: int
+    min_players_num: int
+    max_players_num: int
+    small_blind: int
+    big_blind: int
+
+
+class PasswordRequest(BaseModel):
+    old_password: str
+    new_password: str
 
 
 class NicknameRequest(BaseModel):
@@ -53,6 +56,9 @@ class ActionRequest(BaseModel):
     type: str
     amount: int
 
+class GoogleAuthRequest(BaseModel):
+    token: str
+
 
 @app.get("/", response_class=HTMLResponse)
 def page_home(request: Request):
@@ -60,12 +66,49 @@ def page_home(request: Request):
 
     context = {
         "club_name": "PoluPoker",
-        "creator_name": "Pipunishche",
+        "creator_name": "SiliVal",
         "user": current_user
     }
     return templates.TemplateResponse(
         request=request, name="clear_home.html", context=context
     )
+# -----------------------
+
+@app.post("/api/auth/google")   
+def google_auth_process(request: Request, data: GoogleAuthRequest):
+    print("🔑 Получен Google Token от фронтенда, передаю ядру...")
+
+    auth_url = f"https://{JAVA_HOST}/api/auth/google"
+
+    try:
+        response = requests.post(auth_url, json={"token": data.token}, timeout=3)
+        
+        if response.status_code == 200:
+            user_data = response.json()
+
+            request.session["user"] = {
+                "user_id": str(user_data.get("user_id")),
+                "name": user_data.get("nickname"),
+                "wallet_balance": user_data.get("wallet_balance", 0),
+                "token": user_data.get("access_token"),
+                "refresh_token": user_data.get("refresh_token"),
+                "avatar_url": user_data.get("avatar_filename", "")
+            }
+
+            print(f"✅ Google Auth успешен. Зашел юзер: {user_data.get('nickname')}", flush=True)
+
+            is_new = user_data.get("is_new_user", False)
+            if is_new:
+                return {"status": "success", "is_new_user": True}
+
+            return {"status": "success", "redirect": "/lobby"}         
+        else:
+            print(f"❌ Ядро отклонило Googgle Token: {response.text}", flush=True)
+            return {"error": "Ошибка авторизации"}
+
+    except Exception as e:
+        print(f"❌ Бекенд упал при Google Auth: {e}")
+        return {"error": "Сервер не доступен"}
 # -----------------------
 
 @app.post("/api/upload-avatar")
@@ -77,41 +120,42 @@ async def upload_avatar(request: Request, avatar: UploadFile = File(...)):
     if not avatar.content_type.startswith("image/"):
         return RedirectResponse(url="/profile?error=invalid_file", status_code=303)
 
-    user_id = user.get("user_id")
-    file_ext = avatar.filename.split(".")[-1]
+    file_bytes = await avatar.read()
+    files = {"file": (avatar.filename, file_bytes, avatar.content_type)}
 
-    new_filename = f"{user_id}_{uuid.uuid4().hex[:8]}.{file_ext}"
+    target_url = f"https://{JAVA_HOST}/api/auth/avatar"
+
+    print(f"📤 Перемылаю аватарку {user['name']} в Ядро...", flush=True)
+
+    headers = {"Authorization": f"Bearer {user.get('token')}"}
 
     try:
-        s3_client.upload_fileobj(
-            avatar.file,
-            S3_BUCKET_NAME,
-            new_filename,
-            ExtraArgs={'ContentType': avatar.content_type, 'ACL': 'public-read'}
-        )
-        print(f"☁️ Аватар загружен в облако: {new_filename}", flush=True)
+        response = requests.post(target_url, headers=headers, files=files, timeout=10)
+
+        if response.status_code == 401:
+            request.session.clear()
+            return RedirectResponse(url="/login?error=session_expired", status_code=303)
+
+        if response.status_code == 200:
+            result = response.json()
+            avatar_url = result.get("avatar_url")
+
+            if avatar_url:
+                user["avatar_url"] = avatar_url
+                request.session["user"] = user
+                print(f"✅ Аватар успешно сохранен Ядром. URL: {avatar_url}", flush=True)
+                return RedirectResponse(url="/profile", status_code=303)
+            else:
+                print(f"❌ Ядро вернуло 200, но не преслало avatar_url", flush=True)
+                return RedirectResponse(url="/profile?error=beckend_error", status_code=303)
+        else:
+            print(f"❌ Ядро отклонило файл. Статус: {response.status_code}", flush=True)
+            return RedirectResponse(url="/profile?error=upload_rejected", status_code=303)
+
+
     except Exception as e:
-        print(f"💥 Ошибка загрузки в S3: {e}", flush=True)
-        return RedirectResponse(url="/profile?error=s3_failed", status_code=303)
-
-    public_url = f"{S3_ENDPOINT}/{S3_BUCKET_NAME}/{new_filename}"
-
-    update_url = f"http://{JAVA_HOST}:8080/api/auth/update-avatar"
-    payload = {"user_id": user_id, "avatar_url": public_url}
-
-    java_res = java_request("POST", update_url, request, json_data=payload)
-
-    if not java_res or java_res.status_code == 401:
-        return RedirectResponse(url="/login?error=session_expired", status_code=303)
-
-    if java_res.ok:
-        user["avatar_url"] = public_url
-        request.session["user"] = user
-        print(f"✅ Java принял ссылку на аватар!")
-    else:
-        print(f"❌ Ядро не обновило аватар: {java_res.text}", flush=True)
-
-    return RedirectResponse(url="/profile", status_code=303)
+        print(f"❌ Ошибка связи с ядром при отправке файла: {e}", flush=True)
+        return RedirectResponse(url="/profile?error=server_down", status_code=303)
 # -----------------------
 
 @app.post("/api/profile/nickname")
@@ -121,7 +165,7 @@ def update_nickname(request: Request, data: NicknameRequest):
         return {"redirect": "/login?error=session_expired"}
 
     user_id = user.get("user_id")
-    target_url = f"http://{JAVA_HOST}:8080/api/auth/{user_id}/nickname"
+    target_url = f"https://{JAVA_HOST}/api/auth/{user_id}/nickname"
     payload = {"new_nickname": data.new_nickname}
 
     print(f"🔄 Запрос на смену ника: {user['name']} -> {data.new_nickname}", flush=True)
@@ -150,6 +194,52 @@ def update_nickname(request: Request, data: NicknameRequest):
         return {"error": error_text}
 # -----------------------
 
+@app.post("/api/tables")
+def create_table(request: Request, data: CreateTableRequest):
+    user = request.session.get("user")
+    if not user:
+        return {"redirect": "/login?error=session_expired"}
+
+    user_id = user.get("user_id")
+    target_url = JAVA_URL
+
+    payload = {
+        "name": data.name,
+        "passcode": data.passcode if data.passcode else "",
+        "user_id": user_id,
+        "chips": data.chips,
+        "min_players_num": data.min_players_num,
+        "max_players_num": data.max_players_num,
+        "small_blind": data.small_blind,
+        "big_blind": data.big_blind
+    }
+
+    print(f"🪑 Игрок {user['name']} создает кастомный стол '{data.name}'", flush=True)
+
+    response = java_request("POST", target_url, request, json_data=payload)
+    status = getattr(response, 'status_code', None)
+
+    if not response or status == 401:
+        return {"redirect": "/login?error=session_expired"}
+
+    if status == 200:
+        table_data = response.json()
+        table_name = table_data.get("table_name")
+        table_id = table_data.get("table_id")
+
+        print(f"✅ Стол с именем {table_name} и id: {table_id} успешно создан. Редирект.", flush=True)
+        return {"status": "success", "redirect": f"/table/{table_id}"}
+
+    else:
+        error_text = "Unknown error"
+        try:
+            error_text = response.json().get("error", response.text)
+        except:
+            error_text = response.text
+        print(f"🚫 Java ОТКАЗАЛ В СОЗДАНИИ СТОЛА ({status}): {error_text}", flush=True)
+        return {"error": error_text}
+# -----------------------
+
 @app.get("/lobby", response_class=HTMLResponse)
 def page_lobby(request: Request, error: str = None):
 
@@ -157,7 +247,7 @@ def page_lobby(request: Request, error: str = None):
     if not current_user:
         return RedirectResponse(url="/login", status_code=303)
 
-    balance_url = f"http://{JAVA_HOST}:8080/api/auth/{current_user['user_id']}/balance"
+    balance_url = f"https://{JAVA_HOST}/api/auth/{current_user['user_id']}/balance"
     balance_res = java_request("GET", balance_url, request)
 
     if not balance_res or balance_res.status_code == 401:
@@ -232,76 +322,12 @@ def rebuy_process(table_id: str, request: Request, amount: int = Form(...)):
 # -----------------------
 
 @app.get("/login", response_class=HTMLResponse)
-def page_login(request: Request):
-    return templates.TemplateResponse(request=request, name="login.html")
+def page_login(request: Request, error: str = None):
+    if request.session.get("user"):
+        return RedirectResponse(url="/lobby", status_code=303)
 
-@app.post("/login")
-def login_process(request: Request, login: str = Form(...), password: str = Form(...)): 
-    print(f"🔑 Игрок с логином {login} пытается зайти", flush=True)
-    login_url = f"http://{JAVA_HOST}:8080/api/auth/login"
-
-    try:
-        response = requests.post(login_url, json={"login": login, "password": password}, timeout=3) 
-
-        if response.status_code == 200:         
-            user_data = response.json()
-            
-            request.session["user"] = {
-                "user_id": str(user_data.get("user_id")),
-                "name": user_data.get("nickname"),
-                "wallet_balance": user_data.get("wallet_balance"),
-                "token": user_data.get("access_token"),
-                "refresh_token": user_data.get("refresh_token")
-            }
-
-            print(f"🥷 Игрок {user_data.get('nickname')} успешно залогинился. Данные игрока:", user_data)
-            return RedirectResponse(url="/lobby", status_code=303)
-
-        else: 
-            error_message = "Неверный логин или пароль"
-            return templates.TemplateResponse(request=request, name="login.html", context={"error": error_message})  
-
-    except Exception as e:
-        print(f"🤣 БЕКЕНД УПАЛ ПРИ ЛОГИНЕ: {e}")
-        return templates.TemplateResponse(request=request, name="login.html")
-# -----------------------
-
-@app.get("/register", response_class=HTMLResponse)
-def page_registration(request: Request):
-    return templates.TemplateResponse(request=request, name="registration.html")
-
-@app.post("/register")
-def registration_process(request: Request, nickname: str = Form(...), login: str = Form(...), password: str = Form(...)):
-    print(f"Попытка регистрации с ником: {nickname} и логином {login}") 
-    register_url = f"http://{JAVA_HOST}:8080/api/auth/register"
-
-    try:
-        payload = {"nickname": nickname, "login": login, "password": password} 
-        response = requests.post(register_url, json=payload, timeout=3)
-
-        if response.status_code == 200:
-            user_data = response.json()
-
-            request.session["user"] = {
-                "user_id": str(user_data.get("user_id")),
-                "name": user_data.get("nickname"),
-                "wallet_balance": user_data.get("wallet_balance", 0),
-                "token": user_data.get("access_token"),
-                "refresh_token": user_data.get("refresh_token")
-            }
-            
-            print(f"🥷 Игрок {user_data.get('nickname')} успешно прошел регистрацию. Данные игрока:", user_data)
-            return RedirectResponse(url="/lobby", status_code=303)
-
-        else:
-            error_text = "Ошибка регистрации"
-            try: error_text = response.json().get("error", response.text)
-            except: error_text = response.text
-            return templates.TemplateResponse(request=request, name="registration.html", context={"error": error_text})
-
-    except Exception as e:
-        print(f"🤣 БЕКЕНД УПАЛ ПРИ РЕГИСТРАЦИИ: {e}" )
-        return templates.TemplateResponse(request=request, name="registration.html", context={"error": "Бек-сервер недоступен"})    
+    context = {"v": APP_VERSION, "error": error}
+    return templates.TemplateResponse(request=request, name="login.html", context=context)
 # -----------------------
 
 def java_request(method, url, request: Request, json_data=None, params=None):
@@ -315,7 +341,7 @@ def java_request(method, url, request: Request, json_data=None, params=None):
 
     if res.status_code == 401 and user.get("refresh_token"):
         print(f"🔄 Access Token для {user['name']} истек. Попытка динамического обновления...")
-        refresh_url = f"http://{JAVA_HOST}:8080/api/auth/refresh"
+        refresh_url = f"https://{JAVA_HOST}/api/auth/refresh"
         refresh_payload = {"refresh_token": user.get("refresh_token")}
 
         refresh_res = requests.post(refresh_url, json=refresh_payload, timeout=3)
@@ -380,7 +406,7 @@ def logout(request: Request):
         user_name = user.get('name', 'Unknown')
         print(f"🚪 Игрок с ником {user_name} пытается закончить сессию...", flush=True)
 
-        logout_url = f"http://{JAVA_HOST}:8080/api/auth/logout"
+        logout_url = f"https://{JAVA_HOST}/api/auth/logout"
 
         try:
             logout_response = java_request("POST", logout_url, request, json_data={"user_id": user.get('user_id')})
@@ -423,7 +449,7 @@ def page_table(request: Request, table_id: str, buy_in: int = 0):
 
         if my_id not in current_player_id:
             if buy_in > 0 and request.headers.get("accept") != "application/json":
-                min_required = game_state.get("min_buy_in", 0)
+                min_required = int(game_state.get("min_buy_in", 0))
                 wallet = int(MY_USER.get("wallet_balance", 0))
 
                 if wallet >= min_required:
@@ -566,7 +592,7 @@ def page_profile(request: Request):
         "rank": "Sucker"
     }
 
-    stats_url = f"http://{JAVA_HOST}:8080/api/user/{user_id}/stats"
+    stats_url = f"https://{JAVA_HOST}/api/user/{user_id}/stats"
     stats_res = java_request("GET", stats_url, request)
 
     if stats_res and stats_res.status_code == 200:
